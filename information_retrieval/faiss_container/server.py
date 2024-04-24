6cc14ea2d3f2ffc11026d0941afe9dc03f639cca
slinusc <linus.stuhlmann@gmail.com>
2024-04-24 12:56:08 +0000

    modified faiss container

--- information_retrieval/faiss_container/server.py
+++ information_retrieval/faiss_container/server.py
@@ -6,8 +6,15 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the Faiss index (assuming the index has already been created and saved)
-index = faiss.read_index("/app/faiss_indices/bioBERT_index.index")
-PMIDs = pd.read_csv("/app/PMIDs/bioBERT_pmids.csv", header=None).values.flatten()
+index_path = "/app/faiss_indices/bioBERT_index.index"
+index = faiss.read_index(index_path)
+
+# Load PMIDs and their respective index numbers
+pmids_path = "/app/PMIDs/bioBERT_pmids.csv"
+pmids_df = pd.read_csv(pmids_path)
+
+# Create a dictionary to map index numbers to PMIDs
+index_to_pmids = dict(zip(pmids_df['Index'], pmids_df['PMID']))
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -19,11 +26,13 @@ def search():
     k = int(data['k'])
     
     # Perform the search in the Faiss index
-    distances, i = index.search(queries, k)
-    indices = PMIDs[i]
+    distances, indices = index.search(queries, k)
     
+    # Map the Faiss indices to PMIDs using the dictionary
+    matched_PMIDs = [[index_to_pmids[idx] for idx in row] for row in indices]
+
     # Return the response as JSON
-    return jsonify(indices=indices.tolist(), distances=distances.tolist())
+    return jsonify(PMIDs=matched_PMIDs, distances=distances.tolist())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Accessible over port 5000 on all network interfaces
